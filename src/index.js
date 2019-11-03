f23ed087692c064e2d6bd803fde857cee18af3a0
erfanio <dev@erfan.io>
2019-11-04 10:23:20 +1100

    github pages build

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import App from './app.js';
-import 'normalize.css';
-import './index.css';
-
-const main = () => {
-  new App();
-};
-
-window.addEventListener('load', main);
--- src/utility.js
+++
@@ -1,7 +0,0 @@
-export const range = (start, end) => {
-  const arr = Array(end - start);
-  for (let i = 0; i < end - start; i++) {
-    arr[i] = i+start;
-  }
-  return arr;
-}
